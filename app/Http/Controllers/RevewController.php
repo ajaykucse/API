e34d5e589cd1cafe313822c6a15b2b19a8c2b8fc
ajaykucse <ajaykucse@outlook.com>
2018-12-10 11:57:58 +0545

    Created Model -a

---
+++ app/Http/Controllers/RevewController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Revew;
+use Illuminate\Http\Request;
+
+class RevewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Revew  $revew
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Revew $revew)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\Revew  $revew
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Revew $revew)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Revew  $revew
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Revew $revew)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Revew  $revew
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Revew $revew)
+    {
+        //
+    }
+}
