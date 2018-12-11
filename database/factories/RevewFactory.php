70f8f866be2b9c91bc01e24e1793cd3a3f72b61b
ajaykucse <ajaykucse@outlook.com>
2018-12-11 15:27:20 +0545

    Seeding part for Product and Review

--- database/factories/RevewFactory.php
+++ database/factories/RevewFactory.php
@@ -1,9 +1,20 @@
 <?php
 
+use App\Model\Product;
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Revew::class, function (Faker $faker) {
     return [
-        //
+
+    	'product_id' => function(){
+
+    		return Product::all()->random();
+    	},
+
+        'customer' => $faker->name,
+        
+        'customer' => $faker->paragraph,
+
+        'star' => $faker->numberBetween(0,5)
     ];
 });
