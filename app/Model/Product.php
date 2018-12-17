4c268b11a17fc86b0dc4acdd2b6fdf9e69901b5f
ajaykucse <ajaykucse@outlook.com>
2018-12-17 21:13:04 +0545

    update product

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+	protected $fillable = [
+		'name','details','stock','price','discount'
+	];
     public function reviews()
     {
     	return $this->hasMany(Revew::class);
